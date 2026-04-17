c4d0f9d3870301bf09cd582b6b14bb2829e5e0dd
Jean Carlos GR <jeangarza12@gmail.com>
2026-04-17 01:12:45 -0600

    Se creo el segundo metodo llamado calculadora_cumpleanios

--- Practica1.py
+++ Practica1.py
@@ -9,4 +9,24 @@ class Persona:
     def saludo(self):
         print(f"Hola, soy {self.__nombre}, mi estatura es: {self.__estatura} metros y tengo de Estudios {self.__estudios} ")
 
-    
+    def calculadora_cumpleanios (self):
+        dia_hoy = int(input("Dime el numero del dia de hoy: "))
+        meses = int(input("Dime el numero del mes en el que estas: "))
+
+        total_hoy = meses * 30.4 + dia_hoy
+        total_cumple = self.__mes_cumple * 30.4 + self.__dia_cumple
+
+        if total_cumple < total_hoy:
+            total_cumple += 360  
+
+        faltan_dias = total_cumple - total_hoy
+        opcion = input("¿Quieres saberlo en (dias), (semanas) o (meses)?: ").lower()
+
+        if opcion == "dias":
+            print(f"Faltan {round(faltan_dias)} días.")
+        elif opcion == "semanas":
+            print(f"Faltan {round(faltan_dias / 7)} semanas.")
+        elif opcion == "meses":
+            print(f"Faltan {round(faltan_dias / 30.4, 1)} meses.")
+        else:
+            print("Opción no válida.")
