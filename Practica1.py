2183c2a6d7df3b51d59de43f713fa1b444807ad7
Jean Carlos GR <jeangarza12@gmail.com>
2026-04-17 00:57:08 -0600

    Se creo clase e init

--- Practica1.py
+++ Practica1.py
@@ -1,1 +1,7 @@
-print("Hola mundo")
+class Persona:
+    def __init__(self,nombre,estatura,estudios, dia_cumpleanios,mes_cumpleanios):
+        self.__nombre = nombre
+        self.__estatura = estatura 
+        self.__estudios = estudios
+        self.__dia_cumpleanios = dia_cumpleanios
+        self.__mes_cumpleanios = mes_cumpleanios
