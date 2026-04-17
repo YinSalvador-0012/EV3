d5d41f5444131e642feb8657bd4894192cca92bf
Jean Carlos GR <jeangarza12@gmail.com>
2026-04-17 01:06:18 -0600

    Se creo el primer metodo llamado saludo

--- Practica1.py
+++ Practica1.py
@@ -5,3 +5,8 @@ class Persona:
         self.__estudios = estudios
         self.__dia_cumpleanios = dia_cumpleanios
         self.__mes_cumpleanios = mes_cumpleanios
+
+    def saludo(self):
+        print(f"Hola, soy {self.__nombre}, mi estatura es: {self.__estatura} metros y tengo de Estudios {self.__estudios} ")
+
+    
